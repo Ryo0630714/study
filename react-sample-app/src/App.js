beba7a4e732610436fe04f7034dfd70d3f164f0d
Ryo0630714 <ryos630714@gmail.com>
2025-04-22 22:51:57 +0900

    ractの記述

--- react-sample-app/src/App.js
+++ react-sample-app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const onClicButton = ()=>{alert()};
+  const contentStyle = {
+    color: 'blue',
+    fontSize: '18px'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ color: 'red' }}>こんにちは</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClicButton}>ボタン</button>
+    </>
   );
 }
 
